f12bf90295cfc8932f0c8f0071848f2604ac1fc5
Rishabh <rishabh@Admins-MacBook-Air.local>
2022-09-10 00:58:28 +0530

    redis code

--- mask_qr_caller/Users/models.py
+++ mask_qr_caller/Users/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 # Create your models here.
@@ -7,9 +8,21 @@ class CustomUser(AbstractUser):
     mobile_number = models.CharField(max_length=15,null=True, blank=True)
     display_name = models.CharField(max_length=255, null=True, blank=True)
     qr_uuid = models.CharField(max_length=255, null=True, blank=True)
+    scan_count = models.IntegerField(default=0)
+    called_count = models.IntegerField(default=0)
 
     objects = UserManager()
 
     def __str__(self):
         return self.email
+
+
+
+class MaskNumber(models.Model):
+    cli = models.CharField(max_length=255)
+
+
+class UserNumberMapping(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    cli = models.ForeignKey(MaskNumber, on_delete=models.CASCADE)
 
